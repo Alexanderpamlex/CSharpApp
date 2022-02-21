acd2dd29e6b1cd756127e2fd5fab015b4cc874d0
Александр Лазуткин <Alex125lesy@gmail.com>
2022-02-21 12:34:57 +0300

    sad

---
+++ MyLib/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geometrus.Lazutkin
+{
+    public class Class1{
+       public double arop(double volume) {return (volume * volume * volume) / 2;}
+
+    }
+}
---
+++ MyLib/Square.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geometrus.Lazutkin
+{
+    public class Square
+    {
+        private double side;
+
+        public String toString() { return "Кводрат со стороной=" + side; }
+        public double getSide()
+        {
+            return side;
+        }
+
+        public void setSide(double side)
+        {
+            this.side = side;
+        }
+
+        public double Area() { return side * side; }
+        public double Size() { return side * side * side; }
+    }
+}
